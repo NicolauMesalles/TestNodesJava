f13cce166a6c9991a9fc95a5482958fa058f7727
HamzaEnti <hamza.elhaddad@enti.cat>
2025-12-18 16:11:44 +0100

    V1

--- src/Nodo.java
+++ src/Nodo.java
@@ -2,11 +2,18 @@
 import java.util.ArrayList;
 
 public class Nodo {
-    private String nombre;
-    private ArrayList<Nodo> vecinos;
+    String nombre;
+    String ip;
+    boolean firewall;
+    boolean vulnerable;
+    boolean comprometido;
+    ArrayList<Nodo> vecinos;
 
-    public Nodo(String nombre) {
+    public Nodo(String nombre, String ip, boolean firewall, boolean vulnerable) {
         this.nombre = nombre;
+        this.ip = ip;
+        this.firewall = firewall;
+        this.vulnerable = vulnerable;
         this.vecinos = new ArrayList<>();
     }
 
@@ -18,14 +25,22 @@ public class Nodo {
     public String getNombre() {
         return nombre;
     }
+
+    
     
     public String getVecinos() {
         String tmp = "";
         for (Nodo v : vecinos) {
-            tmp += "[" + v.getNombre() + "] ";
+            tmp += "[" + v.getNombre() + "] - ";
         }
         return tmp;
     }
 
-}
 
+     @Override
+    public String toString() {
+        // TODO: devolver algo tipo:
+        // "WebServer (172.16.0.10) [VULN] [PWN]" etc.
+        return "";
+    }
+}
--- src/Red.java
+++ src/Red.java
@@ -21,8 +21,38 @@ public class Red {
     public void mostrar() {
         System.out.println("=== GRAFO ===");
         for (Nodo n : nodos) {
-            System.out.print(n.getNombre() + " --> " + n.getVecinos() +  "-" + "\n");
+            System.out.print(n.getNombre() + " --> " + n.getVecinos());
         }
     }
 
+     void resetCompromisos() {
+            // TODO: poner comprometido=false a todos los nodos
+    }
+
+    void escanearDesde(Nodo origen) {
+            // TODO: BFS desde origen - https://www.geeksforgeeks.org/dsa/breadth-first-search-or-bfs-for-a-graph/
+            // Reglas:
+            // - si un nodo visitado es vulnerable => comprometido=true
+            // - si un nodo visitado es firewall => NO se propaga a sus vecinos
+    }
+
+    Nodo buscarPorIP(String ip) {
+            // Buscar por IP (iterar nodos)
+            return null;
+    }
+  
+    int contarVulnerablesAlcanzables(Nodo origen) {
+            // Contar vulnerables alcanzables desde origen (BFS)
+            return 0;
+    }
+
+    String listarVecinosDe(Nodo n) {
+            // Listar vecinos (iterar vecinos)
+            return "";
+    }
+
+    ArrayList<Nodo> nodosAislados() {
+            // Obtener nodos aislados (vecinos.size()==0)
+            return new ArrayList<>();
+    }
 }
